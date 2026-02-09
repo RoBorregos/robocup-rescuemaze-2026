edc020518a102fbcd0aba40bf32f572b8c7fa3fa
m2galindo <a00845553@tec.mx>
2026-02-09 13:08:08 -0600

    chande weights

--- Detection/Letters/Last Model.py
+++ Detection/Letters/Last Model.py
@@ -1,35 +1,36 @@
 import cv2
 from ultralytics import YOLO
-#Final model path
-model_path = '/Users/marcoalejandrogalindo/Vision TMR MAZE/runs/detect/modelo_final_HSU/weights/best.pt'
-model = YOLO(model_path)
+from pathlib import Path
 
+
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_PATH = BASE_DIR / "weights" / "best.pt"
+
+model = YOLO(str(MODEL_PATH))
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
 while True:
     ret, frame = cap.read()
-    if not ret: break
-    
+    if not ret:
+        break
+
     results = model.predict(
-        frame, 
-        conf=0.25,         # Confidence threshold
-        device='mps',      # my mac gpu
-        iou=0.5,           # clearer boxes
-        imgsz=640,         # resolution
+        frame,
+        conf=0.1,
+        iou=0.0,
+        imgsz=640,
+        agnostic_nms=True,
         verbose=False
     )
+
     annotated_frame = results[0].plot()
-    
-    cv2.imshow('TMR MAZE - Detección HSU Final', annotated_frame)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("Detector", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-#python "/Users/marcoalejandrogalindo/Vision TMR MAZE/Maze-TMR-VISION-/Detection/HSU/HSU_FINAL.py"
-# # to run this file i use a venv with ultralytics installed
-# source /Users/marcoalejandrogalindo/venv_yolo/bin/activate
-#some times u have to downlawd con tne venv pip install opencv-python ultralytics
