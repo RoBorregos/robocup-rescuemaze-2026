60abd266e2de8f6c30e2b9a1c6f8f243de19628f
m2galindo <a00845553@tec.mx>
2026-03-12 15:36:15 -0600

    Vlx changes

--- include/VLX.h
+++ include/VLX.h
@@ -3,30 +3,30 @@
 #include "Pins_ID.h"
 #include <Adafruit_VL53L0X.h>
 #include <Adafruit_Sensor.h>
-#include "SingleEMAFilter.h"
 #include <Wire.h>
 #include <Arduino.h>
 #include "MUX.h"
+#include "SingleEMAFilter.h"
 
 class VLX{
 private:
     MUX mux_;
     static constexpr uint8_t kMaxInitAttempts_ = 5;
-    static constexpr uint32_t kTimingBudget = 33000;
-    
 public:
-    static constexpr uint8_t kDistanceToWall=15;
+    static constexpr uint8_t kDistanceToWall = 15;
     bool initialized = false;
-    float distance = 0.0f;
+    float distance;
     Adafruit_VL53L0X VLX_ = Adafruit_VL53L0X();
     VL53L0X_RangingMeasurementData_t measure;
-    void updateDistance();
-    float getDistance();
+    
     VLX();
-    VLX(int,int);
-    bool isWall();
     VLX(const uint8_t);
     void setMux(const uint8_t);
     void begin();
+    VLX(int,int);
+    void updateDistance();
+    float getDistance();
+    void printDistance();
+    bool isWall();
 };
 #endif
