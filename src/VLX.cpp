41245a70359bf52287b18af924d1c51678c402a2
Champtiago <a00844931@tec.mx>
2026-03-26 18:34:38 -0600

    Status validation and inter reading time for stabilization

--- src/VLX.cpp
+++ src/VLX.cpp
@@ -2,47 +2,60 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-VLX::VLX() {}
+VLX::VLX(){}
 
-VLX::VLX(const uint8_t posMux) { mux_.setNewChannel(posMux); }
-
-void VLX::begin() {
-  mux_.selectChannel();
-  VLX_.begin();
-  if (!VLX_.begin()) {
-    Serial.println("Error on sensor VL53L0X!");
-    while (1)
-      ;
-  }
-  VLX_.setMeasurementTimingBudgetMicroSeconds(kTimingBudget);
-  VLX_.startRangeContinuous();
-  Serial.println("VL53L0X inicilaized properly.");
+VLX::VLX(const uint8_t posMux){
+    mux_.setNewChannel(posMux);
 }
 
-void VLX::setMux(const uint8_t posMux) { mux_.setNewChannel(posMux); }
-
-float VLX::getDistance() {
-  mux_.selectChannel();
-
-  uint16_t rawRange = VLX_.readRange();
-  if (VLX_.timeoutOccurred()) {
-    // preserve previous valid distance if timeout
-    return distance;
-  }
-
-  distance = (float)(rawRange) / 10.0f;
-  return distance;
+void VLX::begin(){
+    mux_.selectChannel();
+    VLX_.begin();
+    if (!VLX_.begin()) {
+        Serial.println("Error on sensor VL53L0X!"); while (1);
+    }
+    VLX_.setMeasurementTimingBudgetMicroSeconds(kTimingBudget);
+    VLX_.startRangeContinuous(100);
+    Serial.println("VL53L0X inicilaized properly.");  
 }
 
-void VLX::printDistance() {
-  float printedDistance = getDistance();
-  if (printedDistance != 819.0) {
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.println(" mm");
-  } else {
-    Serial.println("Out of range.");
-  }
+void VLX::setMux(const uint8_t posMux) {
+    mux_.setNewChannel(posMux);
 }
 
-bool VLX::isWall() { return (getDistance() < kDistanceToWall); }
+float VLX::getDistance(){
+    mux_.selectChannel();
+
+    uint16_t rawRange = VLX_.readRange();
+    uint16_t status = VLX_.readRangeStatus();
+
+    if (VLX_.timeoutOccurred()) return distance;
+
+    if (status != 4) {
+        distance = (float)(rawRange) / 10.0f;
+        return distance;
+    } else {
+        rawRange = VLX_.readRange();
+        status = VLX_.readRangeStatus();
+        if (status != 4) {
+            distance = (float)(rawRange) / 10.0f;
+            return distance;
+        }
+    }
+    return 819.0f;
+}
+
+void VLX::printDistance(){ 
+    float printedDistance = getDistance();
+    if (printedDistance != 819.0) {
+        Serial.print("Distance: ");
+        Serial.print(distance);
+        Serial.println(" mm");
+    } else {
+        Serial.println("Out of range.");
+    } 
+}
+
+bool VLX::isWall(){
+    return (getDistance()<kDistanceToWall);
+}
